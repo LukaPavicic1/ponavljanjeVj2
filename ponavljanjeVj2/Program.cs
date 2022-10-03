bc33c79077459f71f36425879588ca2e8fc3c699
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-10-03 12:39:05 +0200

    Add files via upload

---
+++ ponavljanjeVj2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanjeVj2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        class Stablo
+        {
+            bool otpadajuListovi;
+            string vrstaStabla;
+
+            public Stablo(bool otpadajuListovi)
+            {
+                this.otpadajuListovi = otpadajuListovi;
+                bool stablo1 = false;
+                bool stablo2 = true;
+            }
+
+            public string OtpadajuListovi { get; set; }
+
+            public override string ToString()
+            {
+                if(otpadajuListovi = true)
+                {
+                    vrstaStabla = "bijelogorično";
+                }
+
+                if (otpadajuListovi = true)
+                {
+                    vrstaStabla = "crnogorično";
+                }
+
+                return base.ToString();
+            }
+
+            
+        }
+    }
+}
